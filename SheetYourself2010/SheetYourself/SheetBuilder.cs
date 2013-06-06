b4ef1f1a8851daa1ce601269100e9fc5719661ff
David T Wilcox <davidtwilcox@gmail.com>
2013-06-06 05:28:38 -0500

    Initial commit
    Adding Visual Studio 2010 and 2012 versions of SheetYourself.

---
+++ SheetYourself2010/SheetYourself/SheetBuilder.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Xml;
+
+namespace SheetYourself
+{
+    /// <summary>
+    /// Provides methods for compiling multiple source images into a single compact sprite sheet.
+    /// </summary>
+    internal class SheetBuilder
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets a value indicating the number of empty pixels to add horizontally
+        /// between images in a sprite sheet.
+        /// </summary>
+        public int HorizontalPadding { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating the number of empty pixels to add vertically
+        /// between images in a sprite sheet.
+        /// </summary>
+        public int VerticalPadding { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Creates a new SheetBuilder instance using the provided value for both
+        /// horizontal and vertical padding.
+        /// </summary>
+        /// <param name="padding">The amount of padding to add both horizontally and
+        /// vertically between images in a sprite sheet.</param>
+        public SheetBuilder(int padding)
+            : this(padding, padding)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new SheetBuilder instance using the provided values for
+        /// horizontal and vertical padding.
+        /// </summary>
+        /// <param name="horizontalPadding">The amount of padding to add horizontally
+        /// between images in a sprite sheet.</param>
+        /// <param name="verticalPadding">The amount of padding to add vertically
+        /// between images in a sprite sheet.</param>
+        public SheetBuilder(int horizontalPadding, int verticalPadding)
+        {
+            HorizontalPadding = horizontalPadding;
+            VerticalPadding = verticalPadding;
+        }
+
+        #endregion
+
+        #region Private methods
+
+        /// <summary>
+        /// Gets an <see cref="ImageInfo"/> instance for the image contained in the specified file.
+        /// If the <paramref name="cropTransparency"/> parameter is true, then transparent pixels around
+        /// all sides of the image are not included when determining the source area of the image.
+        /// </summary>
+        /// <param name="filename">The file name of the image to use.</param>
+        /// <param name="cropTransparency">True if transparent pixels should be removed when determining
+        /// the source area of the image, or false if not.</param>
+        /// <returns>An <see cref="ImageInfo"/> instance for the provided image file.</returns>
+        private ImageInfo GetImageInfo(string filename, bool cropTransparency)
+        {
+            Bitmap image = (Bitmap)Bitmap.FromFile(filename);
+            if (image == null)
+            {
+                throw new ArgumentException(string.Format("Could not open image file '{0}'", filename), "filename");
+            }
+
+            // Lambda method to simplify the process of finding non-transparent pixels below
+            Func<int, int, bool> isTransparent = (px, py) =>
+                {
+                    return (px < image.Width) && (py < image.Height) && (image.GetPixel(px, py).A == 0);
+                };
+
+            int x = 0;
+            int y = 0;
+            int width = image.Width + (2 * HorizontalPadding);
+            int height = image.Height + (2 * VerticalPadding);
+
+            if (cropTransparency)
+            {
+                // Finds the left-most column (X value) containing a non-transparent pixel
+                #region Find left opaque pixel
+                int column = 0;
+                int row = 0;
+                while ((column < image.Width) && isTransparent(column, row))
+                {
+                    while ((row < image.Height) && isTransparent(column, row))
+                    {
+                        ++row;
+                    }
+
+                    if (row == image.Height)
+                    {
+                        row = 0;
+                        ++column;
+                    }
+                }
+                x = column;
+                #endregion
+
+                // Finds the right-most column (X value) containing a non-transparent pixel
+                #region Find right opaque pixel
+                column = image.Width - 1;
+                row = 0;
+                while ((column > x) && isTransparent(column, row))
+                {
+                    while ((row < image.Height) && isTransparent(column, row))
+                    {
+                        ++row;
+                    }
+
+                    if (row == image.Height)
+                    {
+                        row = 0;
+                        --column;
+                    }
+                }
+                width = column - x + 1 + (2 * HorizontalPadding);
+                #endregion
+
+                // Finds the top-most row (Y value) containing a non-transparent pixel
+                #region Find top opaque pixel
+                column = 0;
+                row = 0;
+                while ((row < image.Height) && isTransparent(column, row))
+                {
+                    while ((column < image.Width) && isTransparent(column, row))
+                    {
+                        ++column;
+                    }
+
+                    if (column == image.Width)
+                    {
+                        column = 0;
+                        ++row;
+                    }
+                }
+                y = row;
+                #endregion
+
+                // Finds the bottom-most row (Y value) containing a non-transparent pixel
+                #region Find bottom opaque pixel
+                column = 0;
+                row = image.Height - 1;
+                while ((row > y) && isTransparent(column, row))
+                {
+                    while ((column < image.Width) && isTransparent(column, row))
+                    {
+                        ++column;
+                    }
+
+                    if (column == image.Width)
+                    {
+                        column = 0;
+                        --row;
+                    }
+                }
+                height = row - y + 1 + (2 * VerticalPadding);
+                #endregion
+            }
+
+            return new ImageInfo() { FileName = filename, SourceArea = new Rectangle(x, y, width, height) };
+        }
+
+        /// <summary>
+        /// Creates a sprite sheet image file and XML definition file using the list of <see cref="ImageInfo"/>
+        /// instances provided.
+        /// </summary>
+        /// <param name="outputName">Name to use for the sprite sheet (this is not a file name).</param>
+        /// <param name="sheetName">Name of the sprite sheet image file.</param>
+        /// <param name="xmlName">Name of the sprite sheet XML definition file.</param>
+        /// <param name="size">Size of the final output image used in the sprite sheet.</param>
+        /// <param name="images">List of <see cref="ImageInfo"/> instances to use when building the sprite sheet.</param>
+        private void WriteOutput(string outputName, string sheetName, string xmlName, Size size, List<ImageInfo> images)
+        {
+            Bitmap sheet = new Bitmap(size.Width, size.Height);
+            Graphics graphics = Graphics.FromImage(sheet);
+
+            XmlWriter writer = XmlWriter.Create(xmlName);
+            writer.WriteStartDocument(true);
+            writer.WriteStartElement("spritesheet");
+            writer.WriteElementString("name", outputName);
+            writer.WriteElementString("file", Path.GetFileName(sheetName));
+            writer.WriteElementString("count", images.Count.ToString());
+            writer.WriteElementString("width", size.Width.ToString());
+            writer.WriteElementString("height", size.Height.ToString());
+            writer.WriteStartElement("sprites");
+
+            foreach (ImageInfo info in images)
+            {
+                Bitmap image = (Bitmap)Bitmap.FromFile(info.FileName);
+                graphics.DrawImage(image, new Rectangle(info.Position, info.SourceArea.Size), info.SourceArea, GraphicsUnit.Pixel);
+
+                string spriteName = Path.GetFileNameWithoutExtension(info.FileName).Replace(' ', '_');
+
+                writer.WriteStartElement("sprite");
+                writer.WriteElementString("name", spriteName);
+                writer.WriteElementString("x", info.Position.X.ToString());
+                writer.WriteElementString("y", info.Position.Y.ToString());
+                writer.WriteElementString("width", info.SourceArea.Width.ToString());
+                writer.WriteElementString("height", info.SourceArea.Height.ToString());
+                writer.WriteEndElement();
+            }
+            writer.WriteEndElement();
+            writer.WriteEndElement();
+            writer.WriteEndDocument();
+
+            sheet.Save(sheetName, ImageFormat.Png);
+            graphics.Dispose();
+            sheet.Dispose();
+
+            writer.Close();
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Uses all images found in the specified source folder to build a compacted sprite sheet.
+        /// Images used maintain their original orientation, but may be cropped to remove transparency
+        /// if the <paramref name="cropTransparency"/> parameter is true.
+        /// </summary>
+        /// <param name="sourceFolder">Folder containing images to use in the sprite sheet.
+        /// All images in the folder will be used.</param>
+        /// <param name="outputName">Name to use for the sprite sheet output image file and XML definition file.</param>
+        /// <param name="cropTransparency">True if transparent pixels should be removed when determining
+        /// the source area of the image, or false if not.</param>
+        public void BuildSheet(string sourceFolder, string outputName, bool cropTransparency)
+        {
+            if (!Directory.Exists(sourceFolder))
+            {
+                throw new ArgumentException(string.Format("Source folder '{0}' does not exist", sourceFolder), "sourceFolder");
+            }
+
+            if (string.IsNullOrWhiteSpace(outputName))
+            {
+                throw new ArgumentNullException("outputName");
+            }
+
+            DirectoryInfo dirInfo = new DirectoryInfo(sourceFolder);
+            FileInfo[] files = dirInfo.GetFiles("*.png");
+            if ((files == null) || (files.Length == 0))
+            {
+                // Not finding image files means nothing can be built, but this should not
+                // be treated as an exception
+                return;
+            }
+
+            // Replace spaces in the name with underscores to help with compatibility
+            // with other tools or across platforms.
+            outputName = outputName.Replace(' ', '_');
+            string sheetName = Path.Combine(sourceFolder, outputName + ".png");
+            string xmlName = Path.Combine(sourceFolder, outputName + ".xml");
+            if (File.Exists(sheetName))
+            {
+                File.Delete(sheetName);
+            }
+            if (File.Exists(xmlName))
+            {
+                File.Delete(xmlName);
+            }
+
+            // Calculate the total area of all images and find the widest.
+            // Then create a list of images, sorted from smallest to largest.
+            int totalArea = 0;
+            int maxWidth = 0;
+            List<ImageInfo> images = new List<ImageInfo>();
+            foreach (FileInfo file in files)
+            {
+                if (file.FullName == sheetName)
+                {
+                    continue;
+                }
+                ImageInfo info = GetImageInfo(file.FullName, cropTransparency);
+                images.Add(info);
+
+                totalArea += info.SourceArea.Width * info.SourceArea.Height;
+                maxWidth = (info.SourceArea.Width > maxWidth) ? info.SourceArea.Width : maxWidth;
+            }
+            images.Sort(new ImageInfoSizeComparer());
+            images.Reverse();
+
+            // Target width of the output sprite sheet is the larger of either the square root
+            // of the total area of all images, rounded up, or the width of the widest image.
+            int targetWidth = Math.Max((int)Math.Ceiling(Math.Sqrt(totalArea)), maxWidth);
+            int remainingWidth = targetWidth;
+            int nextX = 0;
+            int nextY = 0;
+            int maxHeight = 0;
+            for (int i = 0; i < images.Count; ++i)
+            {
+                // If the next image being added is wider than the remaining allowed
+                // width of the current row of images, start a new row
+                if (remainingWidth - images[i].SourceArea.Width <= 0)
+                {
+                    remainingWidth = targetWidth;
+                    nextX = 0;
+                    nextY += maxHeight;
+                    maxHeight = 0;
+                }
+                images[i].Position = new Point(nextX, nextY);
+                remainingWidth -= images[i].SourceArea.Width;
+                nextX += images[i].SourceArea.Width;
+                if (images[i].SourceArea.Height > maxHeight)
+                {
+                    maxHeight = images[i].SourceArea.Height;
+                }
+            }
+            Size size = new Size(targetWidth, nextY + maxHeight);
+            WriteOutput(outputName, sheetName, xmlName, size, images);
+        }
+
+        #endregion
+    }
+}
---
+++ SheetYourself2012/SheetYourself/MainForm.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace SheetYourself
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uxFolderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.uxFolderName = new System.Windows.Forms.TextBox();
+            this.uxImageFolderLabel = new System.Windows.Forms.Label();
+            this.uxBrowse = new System.Windows.Forms.Button();
+            this.uxBuild = new System.Windows.Forms.Button();
+            this.uxOutputNameLabel = new System.Windows.Forms.Label();
+            this.uxOutputName = new System.Windows.Forms.TextBox();
+            this.uxCropTransparency = new System.Windows.Forms.CheckBox();
+            this.uxHPadding = new System.Windows.Forms.NumericUpDown();
+            this.uxHPaddingLabel = new System.Windows.Forms.Label();
+            this.uxVPaddingLabel = new System.Windows.Forms.Label();
+            this.uxVPadding = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.uxHPadding)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uxVPadding)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // uxFolderName
+            // 
+            this.uxFolderName.Location = new System.Drawing.Point(86, 6);
+            this.uxFolderName.Name = "uxFolderName";
+            this.uxFolderName.Size = new System.Drawing.Size(320, 20);
+            this.uxFolderName.TabIndex = 0;
+            this.uxFolderName.TextChanged += new System.EventHandler(this.uxFolderName_TextChanged);
+            // 
+            // uxImageFolderLabel
+            // 
+            this.uxImageFolderLabel.AutoSize = true;
+            this.uxImageFolderLabel.Location = new System.Drawing.Point(12, 9);
+            this.uxImageFolderLabel.Name = "uxImageFolderLabel";
+            this.uxImageFolderLabel.Size = new System.Drawing.Size(68, 13);
+            this.uxImageFolderLabel.TabIndex = 1;
+            this.uxImageFolderLabel.Text = "Image Folder";
+            // 
+            // uxBrowse
+            // 
+            this.uxBrowse.Location = new System.Drawing.Point(412, 4);
+            this.uxBrowse.Name = "uxBrowse";
+            this.uxBrowse.Size = new System.Drawing.Size(35, 23);
+            this.uxBrowse.TabIndex = 2;
+            this.uxBrowse.TabStop = false;
+            this.uxBrowse.Text = "...";
+            this.uxBrowse.UseVisualStyleBackColor = true;
+            this.uxBrowse.Click += new System.EventHandler(this.uxBrowse_Click);
+            // 
+            // uxBuild
+            // 
+            this.uxBuild.Enabled = false;
+            this.uxBuild.Location = new System.Drawing.Point(332, 110);
+            this.uxBuild.Name = "uxBuild";
+            this.uxBuild.Size = new System.Drawing.Size(115, 23);
+            this.uxBuild.TabIndex = 0;
+            this.uxBuild.Text = "Build Sprite Sheet";
+            this.uxBuild.UseVisualStyleBackColor = true;
+            this.uxBuild.Click += new System.EventHandler(this.uxBuild_Click);
+            // 
+            // uxOutputNameLabel
+            // 
+            this.uxOutputNameLabel.AutoSize = true;
+            this.uxOutputNameLabel.Location = new System.Drawing.Point(12, 35);
+            this.uxOutputNameLabel.Name = "uxOutputNameLabel";
+            this.uxOutputNameLabel.Size = new System.Drawing.Size(70, 13);
+            this.uxOutputNameLabel.TabIndex = 4;
+            this.uxOutputNameLabel.Text = "Output Name";
+            // 
+            // uxOutputName
+            // 
+            this.uxOutputName.Location = new System.Drawing.Point(86, 32);
+            this.uxOutputName.Name = "uxOutputName";
+            this.uxOutputName.Size = new System.Drawing.Size(320, 20);
+            this.uxOutputName.TabIndex = 0;
+            this.uxOutputName.TextChanged += new System.EventHandler(this.uxOutputName_TextChanged);
+            // 
+            // uxCropTransparency
+            // 
+            this.uxCropTransparency.AutoSize = true;
+            this.uxCropTransparency.Checked = true;
+            this.uxCropTransparency.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.uxCropTransparency.Location = new System.Drawing.Point(290, 59);
+            this.uxCropTransparency.Name = "uxCropTransparency";
+            this.uxCropTransparency.Size = new System.Drawing.Size(116, 17);
+            this.uxCropTransparency.TabIndex = 0;
+            this.uxCropTransparency.Text = "Crop Transparency";
+            this.uxCropTransparency.UseVisualStyleBackColor = true;
+            // 
+            // uxHPadding
+            // 
+            this.uxHPadding.Location = new System.Drawing.Point(114, 58);
+            this.uxHPadding.Maximum = new decimal(new int[] {
+            128,
+            0,
+            0,
+            0});
+            this.uxHPadding.Name = "uxHPadding";
+            this.uxHPadding.Size = new System.Drawing.Size(79, 20);
+            this.uxHPadding.TabIndex = 0;
+            this.uxHPadding.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // uxHPaddingLabel
+            // 
+            this.uxHPaddingLabel.AutoSize = true;
+            this.uxHPaddingLabel.Location = new System.Drawing.Point(12, 60);
+            this.uxHPaddingLabel.Name = "uxHPaddingLabel";
+            this.uxHPaddingLabel.Size = new System.Drawing.Size(96, 13);
+            this.uxHPaddingLabel.TabIndex = 8;
+            this.uxHPaddingLabel.Text = "Horizontal Padding";
+            // 
+            // uxVPaddingLabel
+            // 
+            this.uxVPaddingLabel.AutoSize = true;
+            this.uxVPaddingLabel.Location = new System.Drawing.Point(12, 86);
+            this.uxVPaddingLabel.Name = "uxVPaddingLabel";
+            this.uxVPaddingLabel.Size = new System.Drawing.Size(84, 13);
+            this.uxVPaddingLabel.TabIndex = 9;
+            this.uxVPaddingLabel.Text = "Vertical Padding";
+            // 
+            // uxVPadding
+            // 
+            this.uxVPadding.Location = new System.Drawing.Point(114, 84);
+            this.uxVPadding.Maximum = new decimal(new int[] {
+            128,
+            0,
+            0,
+            0});
+            this.uxVPadding.Name = "uxVPadding";
+            this.uxVPadding.Size = new System.Drawing.Size(79, 20);
+            this.uxVPadding.TabIndex = 0;
+            this.uxVPadding.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 145);
+            this.Controls.Add(this.uxVPadding);
+            this.Controls.Add(this.uxVPaddingLabel);
+            this.Controls.Add(this.uxHPaddingLabel);
+            this.Controls.Add(this.uxHPadding);
+            this.Controls.Add(this.uxCropTransparency);
+            this.Controls.Add(this.uxOutputName);
+            this.Controls.Add(this.uxOutputNameLabel);
+            this.Controls.Add(this.uxBuild);
+            this.Controls.Add(this.uxBrowse);
+            this.Controls.Add(this.uxImageFolderLabel);
+            this.Controls.Add(this.uxFolderName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Sheet Yourself";
+            ((System.ComponentModel.ISupportInitialize)(this.uxHPadding)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uxVPadding)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog uxFolderBrowser;
+        private System.Windows.Forms.TextBox uxFolderName;
+        private System.Windows.Forms.Label uxImageFolderLabel;
+        private System.Windows.Forms.Button uxBrowse;
+        private System.Windows.Forms.Button uxBuild;
+        private System.Windows.Forms.Label uxOutputNameLabel;
+        private System.Windows.Forms.TextBox uxOutputName;
+        private System.Windows.Forms.CheckBox uxCropTransparency;
+        private System.Windows.Forms.NumericUpDown uxHPadding;
+        private System.Windows.Forms.Label uxHPaddingLabel;
+        private System.Windows.Forms.Label uxVPaddingLabel;
+        private System.Windows.Forms.NumericUpDown uxVPadding;
+
+    }
+}
+
